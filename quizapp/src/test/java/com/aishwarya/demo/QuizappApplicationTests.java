911cb797ad5d6d61d9a31a097d829294d090101d
AISHWARYA PANI <paniaishwarya98@gmail.com>
2023-08-06 08:14:57 +0530

    Initial Setup

---
+++ quizapp/src/test/java/com/aishwarya/demo/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aishwarya.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
