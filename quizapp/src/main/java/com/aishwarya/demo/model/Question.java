58458b67eddc6193528862376eb8e449a476b3b5
AISHWARYA PANI <paniaishwarya98@gmail.com>
2023-08-10 01:12:54 +0530

    updated

--- quizapp/src/main/java/com/aishwarya/demo/model/Question.java
+++ quizapp/src/main/java/com/aishwarya/demo/model/Question.java
@@ -12,7 +12,7 @@ import lombok.Data;
 public class Question {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String questionTitle;
     private String option1;
